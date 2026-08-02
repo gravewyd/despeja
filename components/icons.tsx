2704a5296cfa1228c20af157935d0b089d0c4d56
gravewyd <146917003+gravewyd@users.noreply.github.com>
2026-08-02 15:01:55 -0700

    Update Logo component SVG rectangles

--- components/icons.tsx
+++ components/icons.tsx
@@ -9,11 +9,11 @@ const I = (p: SVGProps<SVGSVGElement>, sw = 2) => ({
 export function Logo({ className }: { className?: string }) {
   return (
     <svg viewBox="0 0 100 100" className={className} fill="currentColor" aria-hidden>
-      <rect x="46" y="26" width="8" height="26" rx="4" transform="rotate(0 50 63)" />
-      <rect x="46.25" y="31" width="30" height="31" rx="3.75" transform="rotate(-32 50 63)" />
-      <rect x="46.25" y="31" width="30" height="31" rx="3.75" transform="rotate(32 50 63)" />
-      <rect x="46.5" y="37.5" width="29.5" height="24.5" rx="3.5" transform="rotate(-60 50 63)" />
-      <rect x="46.5" y="37.5" width="29.5" height="24.5" rx="3.5" transform="rotate(60 50 63)" />
+      <rect x="46" y="26" width="30" height="26" rx="4" transform="rotate(0 50 63)" />
+      <rect x="46.25" y="31" width="7" height="21" rx="3.75" transform="rotate(-32 50 63)" />
+      <rect x="46.25" y="31" width=7" height="21" rx="3.75" transform="rotate(32 50 63)" />
+      <rect x="46.5" y="37.5" width="7.5" height="14.5" rx="3.5" transform="rotate(-60 50 63)" />
+      <rect x="46.5" y="37.5" width="29.5" height="14.5" rx="3.5" transform="rotate(60 50 63)" />
       <circle cx="50" cy="63" r="10" />
     </svg>
   );
